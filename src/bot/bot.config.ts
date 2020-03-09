27a265c5219329df2df5029e2af48123d5c14a4c
Gigi <gigi.ho@hm.com>
2020-03-10 01:33:53 +0800

    Fixed bot token config

--- src/bot/bot.config.ts
+++ src/bot/bot.config.ts
@@ -7,6 +7,6 @@ interface Config {
 export default registerAs(
   'bot',
   (): Config => ({
-    token: '1089338339:AAEDA-efvdZc4b8ezmH5yMlHWBOYPw8lREw', //process.env.TELEGRAM_BOT_TOKEN,
+    token: process.env.TELEGRAM_BOT_TOKEN,
   }),
 );
